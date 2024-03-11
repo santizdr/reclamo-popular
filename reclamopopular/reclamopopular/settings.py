5610ee6b469ba166f6b75c3415186026ade97c24
santi <santizuleta11@gmail.com>
2024-03-11 12:33:30 +0100

    Configura base de datos
    Se modifica el archivo de configuracion de Django para usar PostgreSQL como sistema de gestion de base de datos

--- reclamopopular/reclamopopular/settings.py
+++ reclamopopular/reclamopopular/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'reclamopopular.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rp_db',
+        'USER': 'rp_admin',
+        'PASSWORD': 'secret11rp',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        }
     }
-}
+
 
 
 # Password validation
