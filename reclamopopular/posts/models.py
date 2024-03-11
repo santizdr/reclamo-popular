a2f3b2e219f344db2e1da4e8589c2aefbc7cec31
santi <santizuleta11@gmail.com>
2024-03-11 12:24:44 +0100

    Crea modelo de publicaciones

--- reclamopopular/posts/models.py
+++ reclamopopular/posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator 
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(null=False, max_length=255)
+    content = models.TextField(null=False)
+    post_date = models.DateField(auto_now=False, auto_now_add=True)
+    likes = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+
+    def __str__(self):
+        return str(self.title)
