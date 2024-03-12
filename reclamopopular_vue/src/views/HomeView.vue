edc272c4b83609a241f86cb92372175560e4d76e
santi <santizuleta11@gmail.com>
2024-03-12 17:27:36 +0100

    Instala fronend con Vue

---
+++ reclamopopular_vue/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HomePanel from '@/components/HomePanel.vue';
+import TheWelcome from '@/components/HomePanel.vue'
+</script>
+
+<template>
+  <main>
+    <HomePanel />
+  </main>
+</template>
